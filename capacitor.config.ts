d055d9eea11d5ec94e1c3c82d73f8fad9921efcd
Gabriel Martinez <gabriel_martinez@epam.com>
2023-06-15 02:41:29 -0500

    HOTFIX Remove configuration sets for release and debug

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,7 +2,7 @@ import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'io.ionic.automagic',
-  appName: 'automagic',
+  appName: 'Automagic Ally',
   webDir: 'www',
   server: {
     androidScheme: 'https'
