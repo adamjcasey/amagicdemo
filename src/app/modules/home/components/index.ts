53bf9ed628261c5f9f41ee20496315a5990c45a0
Gabriel Martinez <gabriel_martinez@epam.com>
2023-07-09 13:21:52 -0500

    #18 Add Start Dose Ready To Inject video instructive

--- src/app/modules/home/components/index.ts
+++ src/app/modules/home/components/index.ts
@@ -2,15 +2,18 @@ import { StartDosePrepareTempTimerComponent } from './start-dose-prepare-temp-ti
 import { StartDosePrepareSetupComponent } from './start-dose-prepare-setup/start-dose-prepare-setup.component';
 import { StartDosePrepareSurveyComponent } from './start-dose-prepare-survey/start-dose-prepare-survey.component';
 import { StartDosePrepareWaitingToInjectComponent } from './start-dose-prepare-waiting-to-inject/start-dose-prepare-waiting-to-inject.component';
+import { StartDoseReadyToInjectVideoDetailComponent } from './start-dose-ready-to-inject-video-detail/start-dose-ready-to-inject-video-detail.component';
 
 export const components: any[] = [
   StartDosePrepareTempTimerComponent,
   StartDosePrepareSetupComponent,
   StartDosePrepareSurveyComponent,
   StartDosePrepareWaitingToInjectComponent,
+  StartDoseReadyToInjectVideoDetailComponent,
 ];
 
 export * from './start-dose-prepare-survey/start-dose-prepare-survey.component';
 export * from './start-dose-prepare-temp-timer/start-dose-prepare-temp-timer.component';
 export * from './start-dose-prepare-setup/start-dose-prepare-setup.component';
 export * from './start-dose-prepare-waiting-to-inject/start-dose-prepare-waiting-to-inject.component';
+export * from './start-dose-ready-to-inject-video-detail/start-dose-ready-to-inject-video-detail.component';
