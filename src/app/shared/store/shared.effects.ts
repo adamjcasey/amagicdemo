c8dec46bd7e85af89796f23ba3683d3f4a51740c
Gabriel Martinez <gabriel_martinez@epam.com>
2023-07-11 10:38:34 -0500

    #18 Shared effects bugfixing and clean up

--- src/app/shared/store/shared.effects.ts
+++ src/app/shared/store/shared.effects.ts
@@ -13,40 +13,40 @@ export class SharedEffects {
   backdropShow$ = createEffect(() => {
     return this.actions$.pipe(
       ofType(fromActions.ActionTypes.BackdropShow),
+      withLatestFrom(this._store.pipe(select(fromReducer.getBackdropConfig))),
+      map(([action, options]) => {
+        this._backdropOptions = options;
+        return action;
+      }),
       map((action: fromActions.BackdropShow) => action.payload),
       tap((payload) => {
-        if (!this._animationInProgress) {
-          this._animationInProgress = true;
-          this._backdropOptions = payload;
-          if (this._backdropOptions.transition === 'move') {
+        this._backdropOptions = payload;
+        if (this._backdropOptions.transition === 'move') {
+          animate(
+            `#backdrop`,
+            { top: '0px' },
+            { easing: spring({
+              stiffness: 80,
+              damping: 20,
+              mass: 1,
+              velocity: 800,
+            }) }
+          );
+        }
+        else {
+          animate(
+            `#backdrop`,
+            { top: '0px' },
+            { duration: 0 }
+          ).finished.then(() => {
             animate(
-              `#backdrop`,
-              { top: '0px' },
-              { easing: spring({
-                stiffness: 80,
-                damping: 20,
-                mass: 1,
-                velocity: 800,
-              }) }
+              `#backdrop`, 
+              { opacity: [ 0.5, 0.8, 1 ]},
+              { easing: 'ease-in-out', duration: 0.3 }
             ).finished.then(() => {
               this._animationInProgress = false;
             });
-          }
-          else {
-            animate(
-              `#backdrop`,
-              { top: '0px' },
-              { duration: 0 }
-            ).finished.then(() => {
-              animate(
-                `#backdrop`, 
-                { opacity: [ 0.5, 0.8, 1 ]},
-                { easing: 'ease-in-out', duration: 0.3 }
-              ).finished.then(() => {
-                this._animationInProgress = false;
-              });
-            })
-          }
+          })
         }
       })
     )
@@ -60,59 +60,56 @@ export class SharedEffects {
         return action;
       }),
       tap(() => {
-        if (!this._animationInProgress) {
-          this._animationInProgress = true;
-          const elementSize = window.innerHeight * 0.75;
-          if (this._backdropOptions.transition === 'move') {
-            if (this._backdropOptions.fullScreen) {
-              animate(
-                `#backdrop`,
-                {
-                  top: `${(window.innerHeight) * -1}px`,
-                  height: [
-                    `${window.innerHeight}px`,
-                    `${window.innerHeight * 0.9}px`,
-                    `${window.innerHeight * 0.8}px`,
-                    `${window.innerHeight * 0.75}px`
-                  ] 
-                },
-                { easing: spring({
-                  stiffness: 80,
-                  damping: 20,
-                  mass: 1,
-                  velocity: 800,
-                }) },
-              ).finished.then(() => {
-                this._animationInProgress = false;
-              });
-            }
-            else {
-              animate(
-                `#backdrop`,
-                { top: `${(elementSize) * -1}px` },
-                {
-                  easing: 'ease-in-out',
-                  duration: 0.6,
-                } 
-              ).finished.then(() => {
-                this._animationInProgress = false;
-              });
-            }
+        const elementSize = window.innerHeight * 0.75;
+        if (this._backdropOptions.transition === 'move') {
+          if (this._backdropOptions.fullScreen) {
+            animate(
+              `#backdrop`,
+              {
+                top: `${(window.innerHeight) * -1}px`,
+                height: [
+                  `${window.innerHeight}px`,
+                  `${window.innerHeight * 0.9}px`,
+                  `${window.innerHeight * 0.8}px`,
+                  `${window.innerHeight * 0.75}px`
+                ] 
+              },
+              { easing: spring({
+                stiffness: 80,
+                damping: 20,
+                mass: 1,
+                velocity: 800,
+              }) },
+            ).finished.then(() => {
+              this._animationInProgress = false;
+            });
           }
           else {
             animate(
               `#backdrop`,
-              { opacity: [ 0.8, 0.5, 0 ] }, 
-              { duration: 1 }
+              { top: `${(elementSize) * -1}px` },
+              {
+                easing: 'ease-in-out',
+                duration: 0.6,
+              } 
             ).finished.then(() => {
-              animate(
-                `#backdrop`,
-                { top: `${(elementSize) * -1}px` }, 
-              ).finished.then(() => {
-                this._animationInProgress = false;
-              });
-            })
+              this._animationInProgress = false;
+            });
           }
+        }
+        else {
+          animate(
+            `#backdrop`,
+            { opacity: [ 0.8, 0.5, 0 ] }, 
+            { duration: 1 }
+          ).finished.then(() => {
+            animate(
+              `#backdrop`,
+              { top: `${(elementSize) * -1}px` }, 
+            ).finished.then(() => {
+              this._animationInProgress = false;
+            });
+          })
         }
       })
     )
@@ -196,45 +193,31 @@ export class SharedEffects {
   alertShow$ = createEffect(() => {
     return this.actions$.pipe(
       ofType(fromActions.ActionTypes.AlertShow),
+      withLatestFrom(this._store.pipe(select(fromReducer.getAlertConfig))),
+      map(([action, options]) => {
+        this._alertOptions = options;
+        return action;
+      }),
       map((action: fromActions.AlertShow) => action.payload),
-      tap((payload) => {
-        if (!this._animationInProgress) {
-          this._animationInProgress = true;
-          this._alertOptions = payload;
-          const heightOfWindow = window.innerHeight;
-          if (this._alertOptions.mode === 'full') {
-            animate(
-              '#alert .alert__content',
-              { top: [
-                `${heightOfWindow}px`,
-                `${(heightOfWindow * 0.75)}px`,
-                `${(heightOfWindow * 0.50)}px`,
-                `${(heightOfWindow * 0.25)}px`,
-                `0px`,
-              ] },
-              { easing: spring({
-                stiffness: 80,
-                damping: 20,
-                mass: 1,
-                velocity: 800,
-              }) }
-            ).finished.then(() => {
-              this._animationInProgress = false;
-            });
-          }
-          else {
-            // animate(
-            //   `#alert .alert__content`,
-            //   { top: '0px' },
-            //   { duration: 0 }
-            // ).finished.then(() => {
-            //   animate(
-            //     `#backdrop`, 
-            //     { opacity: [ 0.5, 0.8, 1 ]},
-            //     { easing: 'ease-in-out', duration: 0.3 }
-            //   )
-            // })
-          }
+      tap(() => {
+        const heightOfWindow = window.innerHeight;
+        if (this._alertOptions.mode === 'full') {
+          animate(
+            '#alert .alert__content',
+            { top: [
+              `${heightOfWindow}px`,
+              `${(heightOfWindow * 0.75)}px`,
+              `${(heightOfWindow * 0.50)}px`,
+              `${(heightOfWindow * 0.25)}px`,
+              `0px`,
+            ] },
+            { easing: spring({
+              stiffness: 80,
+              damping: 20,
+              mass: 1,
+              velocity: 800,
+            }) }
+          );
         }
       })
     )
@@ -248,34 +231,19 @@ export class SharedEffects {
         return action;
       }),
       tap(() => {
-        if (!this._animationInProgress) {
-          this._animationInProgress = true;
-          if (this._alertOptions.mode === 'full') {
-            animate(
-              `#alert .alert__content`,
-              { top: [0, '25%', '50%', '75%', '100%'] },
-              { easing: spring({
-                stiffness: 80,
-                damping: 20,
-                mass: 1,
-                velocity: 800,
-              }) },
-            ).finished.then(() => {
-              this._animationInProgress = false;
-            });
-          }
-          else {
-            // animate(
-            //   `#backdrop`,
-            //   { opacity: [ 0.8, 0.5, 0 ] }, 
-            //   { duration: 1 }
-            // ).finished.then(() => {
-            //   animate(
-            //     `#backdrop`,
-            //     { top: `${(elementSize) * -1}px` }, 
-            //   )
-            // })
-          }
+        if (this._alertOptions.mode === 'full') {
+          animate(
+            '#alert .alert__content',
+            { top: [0, '25%', '50%', '75%', '100%'] },
+            { easing: spring({
+              stiffness: 80,
+              damping: 20,
+              mass: 1,
+              velocity: 800,
+            }) },
+          ).finished.then(() => {
+            document.querySelector('#alert .alert__content')?.removeAttribute('style');
+          });
         }
       })
     )
