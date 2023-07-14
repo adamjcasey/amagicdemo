774b1642bd20320a4e9290121db26ef8fb184c0a
Adam Casey <adam_casey@epam.com>
2023-07-13 22:48:53 -0400

    Updated according to code review comments

--- src/app/modules/resources/pages/resources.page.ts
+++ src/app/modules/resources/pages/resources.page.ts
@@ -9,6 +9,7 @@ export class ResourcesPage {
 
   public cards_left: Array<any> = [];
   public cards_right: Array<any> = [];
+  public cards: Array<any> = [];
 
 
   constructor() {
@@ -44,6 +45,55 @@ export class ResourcesPage {
     ];
 
     this.cards_right = [
+      {
+        type: 'simple',
+        title: 'Community Feed',
+        asset: '/assets/images/resources-entry-community-feed.svg',
+        description: 'Connect to a larger community of fellow patients to learn tips and tricks.',
+        action: () => {
+          console.log('action go to Community Feed');
+        }
+      },
+      {
+        type: 'simple',
+        title: 'Mindful Assistant',
+        asset: '/assets/images/resources-entry-mindful-assistant.svg',
+        description: 'Use Headspace to help navigate the stress and anxiety of chronic conditions.',
+        action: () => {
+          console.log('action go to Mindful Assistant');
+        }
+      },      
+
+    ];
+
+    this.cards = [
+      {
+        type: 'simple',
+        title: 'Care Team',
+        asset: '/assets/images/resources-entry-care-team.svg',
+        description: 'Connect to your health care providers for holistic care.',
+        action: () => {
+          console.log('action go to Care Team');
+        }
+      },
+      {
+        type: 'simple',
+        title: 'Education',
+        asset: '/assets/images/resources-entry-education.svg',
+        description: 'Stay up-to-date and learn more about your condition and treatment.',
+        action: () => {
+          console.log('action go to Education');
+        }
+      },      
+      {
+        type: 'simple',
+        title: 'OnePath',
+        asset: '/assets/images/resources-entry-one-path.svg',
+        description: 'Chat with your patient support manager (PSM).',
+        action: () => {
+          console.log('action go to OnePath');
+        }
+      },
       {
         type: 'simple',
         title: 'Community Feed',
