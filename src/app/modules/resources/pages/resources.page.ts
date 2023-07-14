3b646957d2ebc834ba653eaee1a8dd9f38698f1d
Adam Casey <adam_casey@epam.com>
2023-07-13 21:56:59 -0400

    Actions added to cards, tied into Resources for navigation

--- src/app/modules/resources/pages/resources.page.ts
+++ src/app/modules/resources/pages/resources.page.ts
@@ -19,18 +19,27 @@ export class ResourcesPage {
         title: 'Care Team',
         asset: '/assets/images/resources-entry-care-team.svg',
         description: 'Connect to your health care providers for holistic care.',
+        action: () => {
+          console.log('action go to Care Team');
+        }
       },
       {
         type: 'simple',
         title: 'Education',
         asset: '/assets/images/resources-entry-education.svg',
         description: 'Stay up-to-date and learn more about your condition and treatment.',
+        action: () => {
+          console.log('action go to Education');
+        }
       },      
       {
         type: 'simple',
         title: 'OnePath',
         asset: '/assets/images/resources-entry-one-path.svg',
         description: 'Chat with your patient support manager (PSM).',
+        action: () => {
+          console.log('action go to OnePath');
+        }
       },
     ];
 
@@ -40,12 +49,18 @@ export class ResourcesPage {
         title: 'Community Feed',
         asset: '/assets/images/resources-entry-community-feed.svg',
         description: 'Connect to a larger community of fellow patients to learn tips and tricks.',
+        action: () => {
+          console.log('action go to Community Feed');
+        }
       },
       {
         type: 'simple',
         title: 'Mindful Assistant',
         asset: '/assets/images/resources-entry-mindful-assistant.svg',
         description: 'Use Headspace to help navigate the stress and anxiety of chronic conditions.',
+        action: () => {
+          console.log('action go to Mindful Assistant');
+        }
       },      
 
     ];
