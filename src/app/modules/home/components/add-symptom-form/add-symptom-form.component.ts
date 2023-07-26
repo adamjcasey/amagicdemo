8889860a0115841f824787caf1140990abd005e6
Gabriel Martinez <gabriel_martinez@epam.com>
2023-07-26 11:12:26 -0500

    TKDSAID-96: Blocking edit of the recorded symptoms.

--- src/app/modules/home/components/add-symptom-form/add-symptom-form.component.ts
+++ src/app/modules/home/components/add-symptom-form/add-symptom-form.component.ts
@@ -25,7 +25,7 @@ export class AddSymptomFormComponent implements OnInit {
   public reportSelected: any;
   public addSymptomFormGroup: FormGroup;
   public symptoms: string[];
-  public editView: boolean = false;
+  public detailView: boolean = false;
 
   constructor(
     private _store: Store<fromCoreStore.CoreState>,
@@ -72,14 +72,14 @@ export class AddSymptomFormComponent implements OnInit {
           this.addSymptomFormGroup.patchValue({
             ...this.reportSelected,
           });
-          this.editView = true;
+          this.detailView = true;
           this._store.dispatch(new fromSharedStore.SliderPageSetContentOptions({
             toolbar: null,
           }));
         }
         else {
           this.reportSelected = null;
-          this.editView = false;
+          this.detailView = false;
         }
       }
     });
