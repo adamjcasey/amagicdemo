aff8e74a8fed9f386c0a94525943a5088278f8b7
Gabriel Martinez <gabriel_martinez@epam.com>
2023-08-24 13:56:59 -0500

    TKDSAID-139: Dicrease delay for free to explore overlay message.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouteReuseStrategy } from '@angular/router';
 
-import { IonicModule, IonicRouteStrategy, GestureController } from '@ionic/angular';
+import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 
 import * as fromCore from '@core/index';
 
