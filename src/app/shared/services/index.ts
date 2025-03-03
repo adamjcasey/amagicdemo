43ebc44174d31c3378d3b5e4f3ecc240746cfc16
Martin Fejes <Martin_Fejes1@epam.com>
2025-03-03 13:10:23 +0100

    TKDAMPB-21    Remove bluetooth service from shared services

--- src/app/shared/services/index.ts
+++ src/app/shared/services/index.ts
@@ -1,10 +1,5 @@
 import { UtilsService } from './utils.service';
-import { BluetoothService } from './bluetooth.service';
 
-export const services: any[] = [
-    UtilsService,
-    BluetoothService,
-];
+export const services: any[] = [UtilsService];
 
 export * from './utils.service';
-export * from './bluetooth.service';
