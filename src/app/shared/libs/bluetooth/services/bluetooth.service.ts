69c3af1869107d84294c38555171b2c3fd82b521
Nikola Drakulic <nikola_drakulic@epam.com>
2025-05-16 13:50:28 +0200

    fix: Welcome screen fixes - TKDAMPB-101, TKDAMPB-102, TKDAMPB-131, TKDAMPB-139

--- src/app/shared/libs/bluetooth/services/bluetooth.service.ts
+++ src/app/shared/libs/bluetooth/services/bluetooth.service.ts
@@ -860,9 +860,9 @@ export class BluetoothService {
         <div class="trouble-connecting-message">
           <h1 class="font-heading-1--bold">Trouble connecting?</h1>
           <img src="assets/images/dosing-trouble-connecting.svg">
-          <p>The injector may need to be reset. <br>Press down on the needle guard <br>until it clicks to reset and release. <br>You do not need to hold.</p>
-          <p>This demo unit does not have a <br>needle nor drug substance.</p>
-          <p>When the injector lights return to <br>solid white, it's ready to continue.</p>
+          <p>The injector may need to be reset. <br>Use the power button to shut off and restart the Aria injector.</p>
+          <p>This demo unit <strong>does not</strong> have a <br>needle nor drug substance.</p>
+          <p>When the injector power lights are <br>on or pulsing, it’s ready to continue.</p>
         </div>
       `,
         buttons: [
