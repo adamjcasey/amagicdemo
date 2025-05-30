993816c3e8afdea5c1a95b899528627e3e5bf13a
Martin Fejes <Martin_Fejes1@epam.com>
2025-05-30 08:13:22 +0200

    TKDAMPB-65    Connect the aria to the closest signal

--- src/app/shared/libs/bluetooth/services/bluetooth.service.ts
+++ src/app/shared/libs/bluetooth/services/bluetooth.service.ts
@@ -746,11 +746,23 @@ export class BluetoothService {
       }
 
       if (this.#devices.length > 0 && !this.#connected) {
-        console.log(
-          'Found devices but not connected, attempting to connect to:',
-          this.#devices[0].device.name
-        );
-        await this.connect(this.#devices[0]);
+        const connectToDevice = async (device: any) => {
+          console.log(
+            'Found devices but not connected, attempting to connect to:',
+            device.device.name
+          );
+
+          await this.connect(device);
+        };
+
+        if (this.#device) {
+          await connectToDevice(this.#device);
+        } else {
+          const closestDevice = this.#devices.sort(
+            (a, b) => (b?.rssi || 0) - (a?.rssi || 0)
+          )[0];
+          await connectToDevice(closestDevice);
+        }
 
         const connected = await firstValueFrom(
           race(
